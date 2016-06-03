43cfa7910fda09f165c33465c6c03288de4f3fe5
alan_de_sousa <al.sousa.93@gmail.com>
2016-06-03 11:25:45 -0300

    Comentando o código mockado

--- Mockei/Mockei.Tests/ProductQueries/SimpleProductTest.cs
+++ Mockei/Mockei.Tests/ProductQueries/SimpleProductTest.cs
@@ -15,26 +15,34 @@ namespace Mockei.Tests.ProductQueries
         [TestMethod]
         public void GetOneProductMocking()
         {
+            
+            //Crio a instancia do Mock para a interface do contexto de banco de dados com base no Entity Framework
             var mockDb = new Mock<IDbContext>();
 
+            //Crio uma lista que é o cenário para o meu Teste
             var list = new List<Product>();
             list.Add(new SimpleProduct() {Amount = 5, Name = "Pedra", ProductId = Guid.NewGuid()});
             list.Add(new SimpleProduct() {Amount = 15, Name = "Café", ProductId = Guid.NewGuid()});
             list.Add(new SimpleProduct() {Amount = 25, Name = "Dragão Branco de Olhos Azuis", ProductId = Guid.NewGuid()});
             list.Add(new SimpleProduct() {Amount = 52, Name = "Tesoura", ProductId = Guid.NewGuid()});
 
+            //Transformar a lista em IQueryable é a forma de setá-la no DbSet posteriormente
             var queryable = list.AsQueryable();
 
+            //Instancio o mock de DbSet que será injetado no mock do Context
             var productMock = new Mock<DbSet<Product>>();
             productMock.As<IQueryable<Product>>().Setup(m => m.Provider).Returns(queryable.Provider);
             productMock.As<IQueryable<Product>>().Setup(m => m.Expression).Returns(queryable.Expression);
             productMock.As<IQueryable<Product>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
             productMock.As<IQueryable<Product>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
 
+            //Seta o objeto do Mock anterior (o mock do dbSet) a chamada do metodo Set
             mockDb.Setup(x => x.Set<Product>()).Returns(productMock.Object);
 
+            //Pega o objeto mockado
             var db = mockDb.Object;
 
+            //testa o objeto mockado
             Assert.AreEqual(4, db.Set<Product>().Count());
         }
     }
